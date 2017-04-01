11c3b738af8e37058d803a0a601a10c73cb25721
lichaohui <158109640@qq.com>
2017-04-01 11:15:12 +0800

    完成promise代替异步回调方案

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,19 @@
 $(function(){
   //获取章节列表的函数
-  function getChapters(start,limit=10){;
-    $.get('data/chapter.json').done(function(){
-      if(data.msg=='success'){
-        if(start<data.chapters.length){
-          var media;
-          $('#more').attr('data-start',start+limit);
-          for(var i=start;i<start+limit;i++){
-            media='<li class="media"><div class="media-body"><h4 class="media-heading"><a href="show.html?cid='+data.chapters[i].id+'">'+data.chapters[i].title+'</a></h4><p>'+data.chapters[i].summary+'</P></div></li>';
-            $('#chapters').append(media);
-          }
-        }else{
-          alert('没有更多了！');
+  function getChapters(start,limit=10){
+    //使用promise方案
+    $.get('data/chapter.json').done(function(data){
+      if(start<data.chapters.length){
+        var media;
+        $('#more').attr('data-start',start+limit);
+        for(var i=start;i<start+limit;i++){
+          media='<li class="media"><div class="media-body"><h4 class="media-heading"><a href="show.html?cid='+data.chapters[i].id+'">'+data.chapters[i].title+'</a></h4><p>'+data.chapters[i].summary+'</P></div></li>';
+          $('#chapters').append(media);
         }
       }
-    }).fail(function(){})
-    /*$.get('data/chapter.json',function(data,status){
-      
-      }
-    })*/
+    }).fail(function(data){
+      alert('列表加载失败，稍后再试');
+    });
   }
   //页面渲染完毕后调用getChapters函数加载一部分章节信息
   getChapters(0,1);
